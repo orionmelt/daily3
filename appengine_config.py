2102468542250ba8169c687282a5d1cf16fd9bd4
Raj <orionmelt@gmail.com>
2014-08-10 10:22:23 -0700

    Enabled remote admin for migration

--- appengine_config.py
+++ appengine_config.py
@@ -15,3 +15,6 @@ def webapp_add_wsgi_middleware(app):
     from google.appengine.ext.appstats import recording
     app = recording.appstats_wsgi_middleware(app)
     return app
+
+remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = (
+    'HTTP_X_APPENGINE_INBOUND_APPID', ['daily3me'])
